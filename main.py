88070ea6900785dc8eef57772009f886e9300f2b
mirecloud <info@mirecloud.com>
2025-09-11 09:53:53 -0400

    adding pydantic

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def read_root():
@@ -9,6 +17,10 @@ def read_root():
 
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...) ):
-    print(payload)
-    return {"new_post": f"title {payload['title']}  content: {payload['content']}"}
+#def create_posts(payload: dict = Body(...) ):
+def create_posts(new_post: Post):
+
+    #print(payload)
+    print(new_post)
+    #return {"new_post": f"title {payload['title']}  content: {payload['content']}"}
+    return {"data": "new_post"}
