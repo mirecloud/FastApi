41b78707d7b58b64742fe1e640f5eb8e84235b2e
mirecloud <info@mirecloud.com>
2025-09-22 20:43:05 -0400

    final

--- SqlAlchemy/main.py
+++ SqlAlchemy/main.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI, Depends, HTTPException
 from database import get_session, init_db, engine
 from sqlalchemy.exc import IntegrityError
 from routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 def on_startup():
